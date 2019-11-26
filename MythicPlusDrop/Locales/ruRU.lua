34c80aff61a3c418389a4922577d4959c953f8c7
BloodDragon2580 <gewinner.thomas@gmail.com>
2019-11-26 12:16:36 +0100

    Update

---
+++ MythicPlusDrop/Locales/ruRU.lua
@@ -0,0 +1,11 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MythicPlusDrop", "ruRU")
+if not L then return end
+
+if L == nil then
+  return
+end
+
+L["FONT"] = "Fonts\\FRIZQT__.TTF"
+
+L["BaseLootLevel"] = "Награда не менее %d уровня предмета."
+L["Keystone_Level"] = "|cff00ff00%s |cffffffffКлюч"
