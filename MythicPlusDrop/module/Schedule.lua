970ff5f5afcd3136c9a255fa4d742e9b19bc7a0e
BloodDragon2580 <gewinner.thomas@gmail.com>
2025-03-11 09:10:20 +0100

    'Update'

--- MythicPlusDrop/module/Schedule.lua
+++ MythicPlusDrop/module/Schedule.lua
@@ -45,13 +45,13 @@ Affix names corresponding to ID
 
 local affixSchedule = {
 	{ [1]=148, [2] =9 , [3]=10, [4]=147, }, -- (1) Xal’atath’s Bargain: Ascendant | Tyrannical | Fortified  | Xal’atath’s Guile
-	{ [1]=159, [2] =10, [3]=9 , [4]=147, }, -- (2) Xal’atath’s Bargain: Oblivion  | Fortified  | Tyrannical | Xal’atath’s Guile
-	{ [1]=158, [2] =9 , [3]=10, [4]=147, }, -- (3) Xal’atath’s Bargain: Voidbound | Tyrannical | Fortified  | Xal’atath’s Guile
+	{ [1]=158, [2] =10, [3]=9 , [4]=147, }, -- (2) Xal’atath’s Bargain: Voidbound | Fortified  | Tyrannical | Xal’atath’s Guile
+ 	{ [1]=162, [2] =9 , [3]=10, [4]=147, }, -- (3) Xal’atath’s Bargain: Pulsar    | Tyrannical | Fortified  | Xal’atath’s Guile
 	{ [1]=160, [2] =10, [3]=9 , [4]=147, }, -- (4) Xal’atath’s Bargain: Devour    | Fortified  | Tyrannical | Xal’atath’s Guile
-	{ [1]=159, [2] =9 , [3]=10, [4]=147, }, -- (5) Xal’atath’s Bargain: Oblivion  | Tyrannical | Fortified  | Xal’atath’s Guile
+	{ [1]=158, [2] =9 , [3]=10, [4]=147, }, -- (5) Xal’atath’s Bargain: Voidbound | Tyrannical | Fortified  | Xal’atath’s Guile
 	{ [1]=148, [2] =10, [3]=9 , [4]=147, }, -- (6) Xal’atath’s Bargain: Ascendant | Fortified  | Tyrannical | Xal’atath’s Guile
 	{ [1]=160, [2] =9 , [3]=10, [4]=147, }, -- (7) Xal’atath’s Bargain: Devour    | Tyrannical | Fortified  | Xal’atath’s Guile
-	{ [1]=158, [2] =10, [3]=9 , [4]=147, }, -- (8) Xal’atath’s Bargain: Voidbound | Fortified  | Tyrannical | Xal’atath’s Guile
+	{ [1]=162, [2] =10, [3]=9 , [4]=147, }, -- (8) Xal’atath’s Bargain: Pulsar    | Fortified  | Tyrannical | Xal’atath’s Guile
 }
 
 local scheduleEnabled = true
@@ -249,7 +249,7 @@ function Mod:Blizzard_ChallengesUI()
 
 		local affixes = {}
 		local prevAffix
-		for j = 5, 1, -1 do
+		for j = 4, 1, -1 do
 			local affix = makeAffix(entry)
 			if prevAffix then
 				affix:SetPoint("RIGHT", prevAffix, "LEFT", -4, 0)
